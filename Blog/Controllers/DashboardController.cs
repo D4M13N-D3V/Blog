4007ab684abe5739a3f2ff2cfff3127bd399bce2
Damien Ostler <damienostler1@outlook.com>
2019-10-13 19:26:16 -0400

    Dashboard template setup

---
+++ Blog/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Models;
+using Blog.Utilities;
+using Microsoft.AspNet.Identity;
+
+namespace Blog.Controllers
+{
+    [RequireHttps]
+    public class DashboardController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
